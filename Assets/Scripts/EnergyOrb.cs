5506b3f59d54578d214219766ed1a230b4e54f1e
sciclunaCynthia <cynthia.scicluna.g51969@mcast.edu.mt>
2026-01-04 18:43:07 +0100

    particle effect when energy orb is grabbed

--- Assets/Scripts/EnergyOrb.cs
+++ Assets/Scripts/EnergyOrb.cs
@@ -20,7 +20,19 @@ public class EnergyOrb : MonoBehaviour
     private void OnGrab(SelectEnterEventArgs args)
     {
         AudioSource.PlayClipAtPoint(pickupSound, transform.position);
-        Instantiate(pickupParticles, transform.position, Quaternion.identity);
+        //Instantiate(pickupParticles, transform.position, Quaternion.identity);
+
+        GameObject particles = Instantiate(
+            pickupParticles,
+            transform.position + Vector3.up * 0.3f,
+            Quaternion.identity
+        );
+
+        ParticleSystem ps = particles.GetComponent<ParticleSystem>();
+        ps.Play();
+
+        Destroy(particles, 2f); //particles finish before orb is destroyed 
+
         ResourceManager.Instance.AddEnergy(energyValue);
         Destroy(gameObject, 0.05f);
 
